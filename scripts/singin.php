3f3a9861222bebbb1e06f7360a3af8fe939a2f29
Andrei Lukashevich <And_Box@iCloud.com>
2023-02-15 22:19:14 +0300

    2023-02-15 at the END

--- scripts/singin.php
+++ scripts/singin.php
@@ -1,9 +1,42 @@
-<!-- <?php echo " <br />" . " SINGIN.PHP " . __DIR__. "<br />";  ?> -->
+<?php
+
+//echo $_REQUEST['user'];
+
+
+
+if (isset($_REQUEST['user'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+    $user = $_POST['user'];
+}
+else {
+    $_REQUEST[] = array();
+    $first_name ='';
+    $last_name ='';
+    $email ='';
+    $user ='';
+}
+
+
+
+?>
+<!-- <?php 
+echo <<<_END
+<button id='but_err' class='button_nav'> HOME  </button>
+ onclick="document.location.href='../index.php'" 
+_END;
+
+
+?> -->
 
 <html>
     
 <head>
+    <!-- <link href="../css/styles.css" rel="stylesheet" type="text/css" /> -->
     <link href="../css/phpmm.css" rel="stylesheet" type="text/css" />
+    <link href="./singin.php" rel="stylesheet" type="text/PHP " />
+    
 </head>
 <body > <!-- bgcolor="#607050"  -->
     <div id="header"><h1>hidden text</h1></div>
@@ -12,47 +45,54 @@
     <div>
         <h1>Регистрация нового пользователя</h1>
         <p> Введите свои данные</p>
-        <form action="getFormSingin" method="POST">
+        <form action="singin.php" method="POST">
             <fieldset>
                 <label for="first_name">Имя:</label>
-                <input type="text" name="first_name" size="20"/><br />
+                <input type="text" name="first_name" size="20" value="<?php echo $first_name; ?>" /><br />
                 <label for="last_name">Фамилия:</label>
-                <input type="text" name="last_name" size="20"/><br />
+                <input type="text" name="last_name" size="20" value="<?php echo $last_name; ?>" /><br />
                 <label for="email">Адрес эл. почты:</label>
-                <input type="text" name="email" size="50"/><br />
-                <label for="user_handel">Никнейм: </label>
-                <input type="text" name="user_handel" size="20"/><br />
+                <input type="text" name="email" size="50" value="<?php echo $email; ?>" /><br />
+                <label for="user">Никнейм: </label>
+                <input type="text" name="user" size="20" value="<?php echo $user; ?>" /><br />
+                <label for=""></label>
+                <input type="text" name="" size=""/><br />
+                <label for=""></label>
+                <input type="text" name="" size="$alue"/><br />
                 <label for=""></label>
                 <input type="text" name="" size=""/><br />
                 <label for=""></label>
                 <input type="text" name="" size=""/><br />
-                <label for=""></label>
-                <input type="text" name="" size=""/><br />
-                <label for=""></label>
-                <input type="text" name="" size=""/><br />
+                <p> </p>
             </fieldset>
+            <br />
             <fieldset class="center">
                 <input type="submit" value="Зарегистрироваться"/>
-                <input type="reset" value="Очистить форму"/>
+                <input type="reset" value="Очистить форму"   />
             </fieldset>
+            <br />
 
-
+            <br />
         </form>
-        <!-- <button id='but_err' class='button_nav' onclick="document.location.href='../index.php'"> HOME </button> -->
+        <br />
+        <form action="../index.php" method='post'>
+            <fieldset  class="center">
+                <?php
+                    echo <<<_END
+                        <button id='but_err' class='button_nav' onclick="document.location.href='../index.php'"> TO HOME </button>
+                        _END;
+                    //echo " <br />" . " SINGIN.PHP " . __DIR__. "<br />";
+                ?>
+            </fieldset>
+        </form>
+
     </div>
 
-
-
-
-<div id="footer"> footer </div>
+    <div id="footer">
+        <p>ERROR</p> 
+    </div>
 </body>
 </html>
 
 
-<?php 
-echo <<<_END
-    <button id='but_err' class='button_nav' onclick="document.location.href='../index.php'"> HOME </button>
-_END;
 
-//echo " <br />" . " SINGIN.PHP " . __DIR__. "<br />";
-?>
