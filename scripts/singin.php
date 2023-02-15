d97cd1e9cbae331500b74645b3cd2a67ccffa774
Andrei Lukashevich <And_Box@iCloud.com>
2023-02-15 09:26:38 +0300

    2023-02-15 At morning

--- scripts/singin.php
+++ scripts/singin.php
@@ -1,28 +1,58 @@
-<!-- <?php
-echo " <br />" . " SINGIN.PHP " . __DIR__. "<br />"; 
-?> -->
+<!-- <?php echo " <br />" . " SINGIN.PHP " . __DIR__. "<br />";  ?> -->
 
 <html>
-<head>
-    <link href="../css/phpMM.css" rel="stylesheep" type="text/css" />
-</head>
-<body bgcolor="#607050">
-    <div id="header"><h1>New user: the missing manual </h1></div>
-    <div id="example"> For new user  </div>
     
-    <div></div>
+<head>
+    <link href="../css/phpmm.css" rel="stylesheet" type="text/css" />
+</head>
+<body > <!-- bgcolor="#607050"  -->
+    <div id="header"><h1>hidden text</h1></div>
+    <div id="example"> New user </div>
+    
+    <div>
+        <h1>Регистрация нового пользователя</h1>
+        <p> Введите свои данные</p>
+        <form action="getFormSingin" method="POST">
+            <fieldset>
+                <label for="first_name">Имя:</label>
+                <input type="text" name="first_name" size="20"/><br />
+                <label for="last_name">Фамилия:</label>
+                <input type="text" name="last_name" size="20"/><br />
+                <label for="email">Адрес эл. почты:</label>
+                <input type="text" name="email" size="50"/><br />
+                <label for="user_handel">Никнейм: </label>
+                <input type="text" name="user_handel" size="20"/><br />
+                <label for=""></label>
+                <input type="text" name="" size=""/><br />
+                <label for=""></label>
+                <input type="text" name="" size=""/><br />
+                <label for=""></label>
+                <input type="text" name="" size=""/><br />
+                <label for=""></label>
+                <input type="text" name="" size=""/><br />
+            </fieldset>
+            <fieldset class="center">
+                <input type="submit" value="Зарегистрироваться"/>
+                <input type="reset" value="Очистить форму"/>
+            </fieldset>
+
+
+        </form>
+        <!-- <button id='but_err' class='button_nav' onclick="document.location.href='../index.php'"> HOME </button> -->
+    </div>
 
 
 
 
-<div id="footer"></div>
+<div id="footer"> footer </div>
 </body>
 </html>
 
 
 <?php 
-echo " <br />" . " SINGIN.PHP " . __DIR__. "<br />";
 echo <<<_END
-    <button id='but_err'    class='button_nav' onclick="document.location.href='../../shop/index.php'"> HOME </button>
+    <button id='but_err' class='button_nav' onclick="document.location.href='../index.php'"> HOME </button>
 _END;
+
+//echo " <br />" . " SINGIN.PHP " . __DIR__. "<br />";
 ?>
