9213e978b9e4c4b80e81a164417a1e37349ebc8c
Andrei Lukashevich <And_Box@iCloud.com>
2023-03-02 21:07:48 +0300

    Netbeans 2023-03-02 jQuery ajax remove users

--- scripts/show_users.php
+++ scripts/show_users.php
@@ -2,10 +2,10 @@
 session_start();
 require_once 'functions.php';
 
-if (!isset( $_SESSION['user_id'])) {
-    header('Location:'. "../index.php" . "?user_id=" );
-    exit();
-}
+//if (!isset( $_SESSION['user_id'])) {
+//    header('Location:'. "../index.php" . "?user_id=" );
+//    exit();
+//}
 if (! user_in_group($_SESSION['user_id'], 'Administrator')) {
     header('Location:'. "../index.php" . "?user_id=" );
     exit();
@@ -61,25 +61,36 @@ _END;
     $(function(){
         $("img[class='del_user']").click( function(){
             var $this = $(this);
-            if(window.confirm('You ar goin to DELETE:\n\t "'  + this.alt + '"\n\n Are You sure? Press OK')){
-                //$this.attr('src', '../sources/images/check_false.jpg');
-
+            if(window.confirm('You ar goin to DELETE:\n\t "'  + this.alt + '"\n\n Are You sure? Press OK')){  //CONFIRM
+                //$this.attr('src', '../sources/images/check_false.jpg');\
+                
+                //var $this ;
                 var user_id = this.id;
                 var param = {user_id: user_id};
                 var user_name = this.alt;
                 
                 $.ajax({
-                    url: 'delete_user.php',
+                    url: 'remove_user.php',
                     type: 'POST',
                     data: param,
                     contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
                     success: function (response){
                         $('#attention').html(response);
+
+                        var element = $this.parent();
+                        element.remove();
                         
                     },
-                    error: function (){ alert("ERROR");}
+                    error: function (){
+                        alert("ERROR");
+                        
+                        
+                    }
                 });
-            }
+                
+
+                
+            }  //CONFIRM
         });
     });
 
@@ -89,49 +100,52 @@ _END;
     <div>
         <h1> Users </h1>
         <p id="attention"> &nbsp; </p>
-        
-<!--  <p> <?php echo '$user_id not work this' ; ?>  </p>-->
-<!--  <p> <?php echo '$user_name not work this' ; ?> </p>-->
-        
         <div id="content">
+
             <ul>
             <?php
             foreach ($result as $key  ) {
-                $user_row = sprintf(
+//                $user_row = sprintf(
+//                        "<li>"
+//                        . "<a   alt='%d' href='show_user.php?user_id='  > %s %s &nbsp; &nbsp; Login: %s &nbsp; &nbsp; Password: %s </a>"
+//                        . "<i>&nbsp;&nbsp;&nbsp;</i>"
+//                        . "<img id='%s' class='free_user' src='../sources/images/check_true.jpg' width='20' alt='%s %s' /> "
+//                        . "<i>&nbsp; &nbsp;</i>"
+//                        . "<img id='%s' class='del_user'  src='../sources/images/check_del.jpg'  width='22' alt='%s %s' /> "
+//                        . "<br>"
+//                        . "</li>"
+//                        . '<br>', 
+//                        
+//                        $key['user_id'], $key['first_name'], $key['last_name'], $key['user_name'], $key['password'],
+//                        $key['user_id'], $key['first_name'], $key['last_name'],
+//                        $key['user_id'], $key['first_name'], $key['last_name']
+//                        );
+                $user_row = strtr(
                         "<li>"
-                        . "<a href='show_user.php?user_id=%d'> %s %s &nbsp; &nbsp; Login: %s &nbsp; &nbsp; Password: %s </a>"
+                        . "<a   id='%user_id' href='show_user.php?user_id='  > %first_name %last_name &nbsp; &nbsp; Login: %user_name &nbsp; &nbsp; Password: %password </a>"
                         . "<i>&nbsp;&nbsp;&nbsp;</i>"
-                        . "<img class='free_user' id='%s' src='../sources/images/check_true.jpg' width='20' alt='%s %s' /> "
+                        . "<img id='%user_id' class='free_user' src='../sources/images/check_true.jpg' alt='%first_name %last_name' width='20' /> "
                         . "<i>&nbsp; &nbsp;</i>"
-                        . "<img class='del_user' id='%s' src='../sources/images/check_del.jpg' width='22' alt='%s %s' /> "
+                        . "<img id='%user_id' class='del_user'  src='../sources/images/check_del.jpg'  alt='%first_name %last_name' width='22' /> "
                         . "<br>"
-                        . "</li>" . '<br>',
-                        $key['user_id'], $key['first_name'], $key['last_name'], $key['user_name'], $key['password'],
-                        $key['user_id'],$key['first_name'], $key['last_name'],
-                        $key['user_id'],$key['first_name'], $key['last_name']
-                        );
+                        . "</li>"
+                        . '<br>' ,
+                        [ '%user_id'    => $key['user_id'],
+                          '%first_name' => $key['first_name'],
+                          '%last_name'  => $key['last_name'],
+                          '%user_name'  => $key['user_name'],
+                          '%password'   => $key['password'] ] ); //    ,
+                       // $to);
                 echo $user_row ;
 
-                // echo $key['user_pic_path'] . '  ';
-                // echo $key['image_id']     . '  '. '<br>';
+
             }
-            //print_r($result); 
-            //var_dump($result);
+
             ?>
             </ul>
         </div>
         
         <br />
-<!--        <form action="../index.php" method='post'>
-            <fieldset  class="center">
-                <?php
-                   // echo <<<_END
-                   //     <button id='but_err' class='button_nav' onclick="document.location.href='../index.php'"> TO HOME </button>
-                   //    _END;
-                  
-                ?>
-            </fieldset>
-        </form>-->
 
     </div>
 
